7879be5b7a9b9551bad02dae429557dcdf9ea7de
中嶋　慎也 <nakasin52@gmail.com>
2019-02-06 13:49:27 +0900

    Unitについて

--- hello.kt
+++ hello.kt
@@ -1,8 +1,15 @@
-fun sum(a: Int, b: Int): Int {
+fun sum(a: Int, b: Int) :Int{
     return a + b
+} 
+// fun printSum(a: Int, b: Int): Unit {
+//     println("sum of $a and $b is ${a + b}")
+// }
+
+fun printSum(a: Int, b: Int) {
+    println("sum of $a and $b is ${a + b}")
 }
 
 fun main() {
-    print("sum of 3 and 5 is ")
-    println(sum(3, 5))
+    println("sum of 19 and 23 is ${sum(19, 23)}")
+    printSum(-1, 8)
 }
